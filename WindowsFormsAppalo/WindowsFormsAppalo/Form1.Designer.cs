ce54346efcbf711c9b684607753ef18d16073d8e
Manh Pham <manhvamanh12@gmail.com>
2022-03-06 10:45:03 +0700

    manhtruong

--- WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
+++ WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
@@ -51,9 +51,10 @@ namespace WindowsFormsAppalo
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(101, 127);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(84, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "manhtruong";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
