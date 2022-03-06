0ea67ee468586775e63bbd8b3a8392aa8bb7c5e8
Manh Truong <truongnguyen0777@gmail.com>
2022-03-06 10:52:10 +0700

    commit

--- WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
+++ WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
@@ -35,15 +35,16 @@ namespace WindowsFormsAppalo
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(324, 334);
+            this.button1.Location = new System.Drawing.Point(176, 333);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(172, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "dang nhap";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -88,11 +89,21 @@ namespace WindowsFormsAppalo
             this.label3.TabIndex = 5;
             this.label3.Text = "con me may";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(470, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "thoat";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -114,6 +125,7 @@ namespace WindowsFormsAppalo
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
