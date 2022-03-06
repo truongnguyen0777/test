4e9ff8ad46e31fccf4e50cf13362e0883a4bd7b2
Manh Truong <truongnguyen0777@gmail.com>
2022-03-06 10:39:48 +0700

    commit

--- WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
+++ WindowsFormsAppalo/WindowsFormsAppalo/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace WindowsFormsAppalo
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -77,11 +78,21 @@ namespace WindowsFormsAppalo
             this.textBox2.Size = new System.Drawing.Size(319, 22);
             this.textBox2.TabIndex = 4;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "con me may";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -101,6 +112,7 @@ namespace WindowsFormsAppalo
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
